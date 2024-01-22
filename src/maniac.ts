6a497b11ec4398a28850be53145ca996f1d41265
Neville Brem <bremneville@gmail.com>
2024-01-22 13:54:29 +0100

    init: first commit

---
+++ src/maniac.ts
@@ -0,0 +1,101 @@
+import type { CustomThemeConfig } from '@skeletonlabs/tw-plugin';
+
+export const maniac: CustomThemeConfig = {
+	name: 'maniac',
+	properties: {
+		// =~= Theme Properties =~=
+		'--theme-font-family-base': `system-ui`,
+		'--theme-font-family-heading': `system-ui`,
+		'--theme-font-color-base': '0 0 0',
+		'--theme-font-color-dark': '255 255 255',
+		'--theme-rounded-base': '12px',
+		'--theme-rounded-container': '12px',
+		'--theme-border-base': '2px',
+		// =~= Theme On-X Colors =~=
+		'--on-primary': '0 0 0',
+		'--on-secondary': '255 255 255',
+		'--on-tertiary': '0 0 0',
+		'--on-success': '0 0 0',
+		'--on-warning': '0 0 0',
+		'--on-error': '0 0 0',
+		'--on-surface': '255 255 255',
+		// =~= Theme Colors  =~=
+		// primary | #3772FF
+		'--color-primary-50': '225 234 255', // #e1eaff
+		'--color-primary-100': '215 227 255', // #d7e3ff
+		'--color-primary-200': '205 220 255', // #cddcff
+		'--color-primary-300': '175 199 255', // #afc7ff
+		'--color-primary-400': '115 156 255', // #739cff
+		'--color-primary-500': '55 114 255', // #3772FF
+		'--color-primary-600': '50 103 230', // #3267e6
+		'--color-primary-700': '41 86 191', // #2956bf
+		'--color-primary-800': '33 68 153', // #214499
+		'--color-primary-900': '27 56 125', // #1b387d
+		// secondary | #4F46E5
+		'--color-secondary-50': '229 227 251', // #e5e3fb
+		'--color-secondary-100': '220 218 250', // #dcdafa
+		'--color-secondary-200': '211 209 249', // #d3d1f9
+		'--color-secondary-300': '185 181 245', // #b9b5f5
+		'--color-secondary-400': '132 126 237', // #847eed
+		'--color-secondary-500': '79 70 229', // #4F46E5
+		'--color-secondary-600': '71 63 206', // #473fce
+		'--color-secondary-700': '59 53 172', // #3b35ac
+		'--color-secondary-800': '47 42 137', // #2f2a89
+		'--color-secondary-900': '39 34 112', // #272270
+		// tertiary | #0EA5E9
+		'--color-tertiary-50': '219 242 252', // #dbf2fc
+		'--color-tertiary-100': '207 237 251', // #cfedfb
+		'--color-tertiary-200': '195 233 250', // #c3e9fa
+		'--color-tertiary-300': '159 219 246', // #9fdbf6
+		'--color-tertiary-400': '86 192 240', // #56c0f0
+		'--color-tertiary-500': '14 165 233', // #0EA5E9
+		'--color-tertiary-600': '13 149 210', // #0d95d2
+		'--color-tertiary-700': '11 124 175', // #0b7caf
+		'--color-tertiary-800': '8 99 140', // #08638c
+		'--color-tertiary-900': '7 81 114', // #075172
+		// success | #618c3f
+		'--color-success-50': '231 238 226', // #e7eee2
+		'--color-success-100': '223 232 217', // #dfe8d9
+		'--color-success-200': '216 226 207', // #d8e2cf
+		'--color-success-300': '192 209 178', // #c0d1b2
+		'--color-success-400': '144 175 121', // #90af79
+		'--color-success-500': '97 140 63', // #618c3f
+		'--color-success-600': '87 126 57', // #577e39
+		'--color-success-700': '73 105 47', // #49692f
+		'--color-success-800': '58 84 38', // #3a5426
+		'--color-success-900': '48 69 31', // #30451f
+		// warning | #d153fc
+		'--color-warning-50': '248 229 255', // #f8e5ff
+		'--color-warning-100': '246 221 254', // #f6ddfe
+		'--color-warning-200': '244 212 254', // #f4d4fe
+		'--color-warning-300': '237 186 254', // #edbafe
+		'--color-warning-400': '223 135 253', // #df87fd
+		'--color-warning-500': '209 83 252', // #d153fc
+		'--color-warning-600': '188 75 227', // #bc4be3
+		'--color-warning-700': '157 62 189', // #9d3ebd
+		'--color-warning-800': '125 50 151', // #7d3297
+		'--color-warning-900': '102 41 123', // #66297b
+		// error | #d719b5
+		'--color-error-50': '249 221 244', // #f9ddf4
+		'--color-error-100': '247 209 240', // #f7d1f0
+		'--color-error-200': '245 198 237', // #f5c6ed
+		'--color-error-300': '239 163 225', // #efa3e1
+		'--color-error-400': '227 94 203', // #e35ecb
+		'--color-error-500': '215 25 181', // #d719b5
+		'--color-error-600': '194 23 163', // #c217a3
+		'--color-error-700': '161 19 136', // #a11388
+		'--color-error-800': '129 15 109', // #810f6d
+		'--color-error-900': '105 12 89', // #690c59
+		// surface | #080708
+		'--color-surface-50': '218 218 218', // #dadada
+		'--color-surface-100': '206 205 206', // #cecdce
+		'--color-surface-200': '193 193 193', // #c1c1c1
+		'--color-surface-300': '156 156 156', // #9c9c9c
+		'--color-surface-400': '82 81 82', // #525152
+		'--color-surface-500': '8 7 8', // #080708
+		'--color-surface-600': '7 6 7', // #070607
+		'--color-surface-700': '6 5 6', // #060506
+		'--color-surface-800': '5 4 5', // #050405
+		'--color-surface-900': '4 3 4' // #040304
+	}
+};
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { AppShell, storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<nav class="flex justify-between p-4">
+			<a href="/" class="h1 font-bold italic">logo</a>
+			<div class="flex gap-8 font-semibold my-auto h4">
+				<a href="/pricing">Pricing</a>
+				<a href="/pricing">Features</a>
+			</div>
+			<a href="https://app.taskforge.com" class="btn variant-ringed-primary font-bold">Dashboard</a>
+		</nav>
+	</svelte:fragment>
+	<slot />
+</AppShell>
